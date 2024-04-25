fc1f2791f9b3f00e8917e9e154cf4875f7fc04d2
Daniel Alejandro Flores Sepulveda <160781340+IngIktan@users.noreply.github.com>
2024-04-25 12:00:05 -0600

    Percepción

---
+++ _087_Probabilidad_Reconocimiento de Escritura_V00.py
@@ -0,0 +1,37 @@
+# Autor: Daniel Alejandro Flores Sepulveda
+# Este programa realiza el reconocimiento de dígitos escritos a mano utilizando una red neuronal convolucional (CNN) con TensorFlow.
+
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+
+# Cargar y preprocesar el conjunto de datos MNIST
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Construir el modelo de la red neuronal convolucional (CNN)
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+# Compilar el modelo
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entrenar el modelo
+model.fit(x_train, y_train, epochs=5, batch_size=64, validation_data=(x_test, y_test))
+
+# Evaluar el modelo
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print("Accuracy on test data:", test_accuracy)
