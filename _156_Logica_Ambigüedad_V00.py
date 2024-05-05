b67e8bec4c1287ebb595a86c125cb6ca34a4ffff
Daniel Alejandro Flores Sepulveda <160781340+IngIktan@users.noreply.github.com>
2024-05-04 21:19:05 -0600

    Tratamiento Lógico del Lenguaje

---
+++ _156_Logica_Ambigüedad_V00.py
@@ -0,0 +1,8 @@
+# Autor: Daniel Alejandro Flores Sepulveda
+# Descripción: Ejemplo de ambigüedad en un enunciado.
+
+enunciado = "Vi a la mujer con el telescopio"
+print("Enunciado:", enunciado)
+
+# Interpretación 1: Vi a la mujer usando el telescopio
+# Interpretación 2: Vi a la mujer a través del telescopio
